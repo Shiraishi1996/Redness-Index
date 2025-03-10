da8a8892028f3ed6017978d5d42bc4facf9e1b4e
catty <86176561+Shiraishi1996@users.noreply.github.com>
2025-03-10 17:37:00 +0900

    Update open_source.py

--- open_source.py
+++ open_source.py
@@ -1112,11 +1112,11 @@ if st.session_state["button_clicked"]:
                     st.success("データが送信されました！")
                     m = image_maker(start_date=str(date),end_date=str(date2),lat_mean=lat_mean, lon_mean=lon_mean,max_value=max_value)
 
-                    html_data = io.StringIO()  
-                    m.save(html_data)  # HTML を保存
-                    html_str = html_data.getvalue()  # そのまま文字列として取得
-                    html_data.close()  # データを取得した後に明示的に閉じる
-
+                    html_data = io.BytesIO()
+                    m.save(html_data)  # Folium の `save()` は bytes を扱う
+                    html_str = html_data.getvalue().decode("utf-8")  # `bytes` を `str` に変換
+                    html_data.close()  # データ取得後に閉じる
+                                        
                     # Streamlit アプリ
                     st.title("Folium Map Download Example")
                     st_folium(m, width=700, height=500)
